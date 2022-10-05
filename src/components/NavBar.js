323ae11403f2d174cc7b647e4df1cb113d4784b3
Derrick Nyasio <dnyasio2014@gmail.com>
2022-10-05 11:57:05 +0300

    Passed tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,12 @@ import React from "react";
 
 function NavBar() {
     const links = ["home", "about", "projects"];
+    const updatedLinks = links.map((link) => ( <
+        a key = { link }
+        href = { `#` + link } > { link } < /a>
+    ));
 
-    return <nav > {
-            links.map(link => ( < >
-                <
-                a key = { link }
-                href = { '#' + link } > { link } < /a>  <
-                />
-            ))
-        } <
+    return <nav > { updatedLinks } <
         /nav>;
 }
 
